98607ccfabfaa04a38b7156a8cd860561473e2aa
Oren Partesana <80120095+oren-p-bgu@users.noreply.github.com>
2022-11-16 02:51:16 +0200

    Implemented everything

--- Game/game.cpp
+++ Game/game.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <glm/gtc/matrix_transform.hpp>
 #include <stb_image.c>
-
+#include <numbers>
+# define PI           3.14159265358979323846  /* pi */
 static void printMat(const glm::mat4 mat)
 {
 	std::cout<<" matrix:"<<std::endl;
@@ -38,8 +39,8 @@ void Game::Init()
 
 	unsigned char* grayscaled = Grayscale(width,height,data);
 	unsigned char* edges = EdgeDetection(width,height, grayscaled);
-	unsigned char* halftoned = Halftones(width,height,data);
-	unsigned char* fsalgo = FSAlgorithm(width,height,data);
+	unsigned char* halftoned = Halftones(width,height,grayscaled);
+	unsigned char* fsalgo = FSAlgorithm(width,height, grayscaled);
 
 	AddTexture(width, height, grayscaled);
 	AddTexture(width, height, edges);
@@ -48,6 +49,8 @@ void Game::Init()
 
 	free(grayscaled);
 	free(edges);
+	free(halftoned);
+	free(fsalgo);
 
 	AddShape(Plane,-1,TRIANGLES);
 	
@@ -78,18 +81,130 @@ unsigned char* Game::Grayscale(int width, int height, unsigned char* data)
 
 unsigned char* Game::EdgeDetection(int width, int height, unsigned char* data)
 {
+	int sobelXMatrix[3][3] = {	{1,0,-1}, 
+											{2,0,-2},
+											{1,0,-1} };
+
+	int sobelYMatrix[3][3] = {	{1,2,1}, 
+											{0,0,0},
+											{-1,-2,-1} };
+
+	int lowThreshold = 0.9 * 255;
+// Make a 2d version of the data for easier handling.	
+	std::vector<std::vector<int>> data2d(height, std::vector<int> (width));
+
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			data2d[row][col] = data[row * width * 4 + col * 4];
+		}
+	}
+
+	std::vector<std::vector<int>> gradientMatrix(height, std::vector<int> (width));
+	std::vector<std::vector<int>> edgesMatrix(height, std::vector<int> (width));
+
+	// GradientDirectionMatrix in degrees.
+	std::vector<std::vector<int>> gradientDirectionMatrix(height, std::vector<int> (width));
+
+	int max_gradient = 0;
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			int sumX = 0;
+			int sumY = 0;
+			for (int i = -1; i < 2; i++) {
+				for (int j = -1; j < 2; j++) {
+					if (i == 0 && j == 0) {
+						continue;
+					}
+					int x = row + i;
+					int y = col + j;
+					if (x < 0 || x > width - 1 || y < 0 || y > height - 1) {
+						continue;
+					}
+
+					sumX = sumX + sobelXMatrix[i + 1][j + 1] * data2d[x][y];
+					sumY = sumY + sobelYMatrix[i + 1][j + 1] * data2d[x][y];
+				}
+			}
+
+
+			int gradient = sqrt(sumX * sumX + sumY * sumY);
+			if (gradient > max_gradient) {
+				max_gradient = gradient;
+			}
+			gradientMatrix[row][col] = gradient;
+
+			int gradientDirection = atan2(sumY, sumX);
+			gradientDirectionMatrix[row][col] = trunc(gradientDirection * 180 / PI + 0.5);
+		}
+	}
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			if (gradientMatrix[row][col] < lowThreshold) {
+				edgesMatrix[row][col] = 0;
+				continue;
+			}
+
+			int prev = 0;
+			int next = 0;
+			int direction = gradientDirectionMatrix[row][col];
+			int intensity = gradientMatrix[row][col];
+			// This section maps a direction and rounds it to the nearest cardinal direction to compare to the 
+			// correct pixels and find the local maximum.
+			// Direction East-West
+			if (direction <= -157.5 || (direction >= -22.5 && direction <= 22.5) || (direction >= 157.5) ) {
+				if (col + 1 < width && intensity >= gradientMatrix[row][col + 1]) {
+					next = gradientMatrix[row][col + 1];
+				}
+				if (col != 0 && intensity >= gradientMatrix[row][col -1]) {
+					prev = gradientMatrix[row][col - 1];
+				}
+			}
+// Direction North West-South East
+			if ((direction >= 22.5 && direction <= 67.5) || (direction >= -157.5 && direction <= -112.5) ) {
+				if (col + 1 < width && row != 0 && intensity >= gradientMatrix[row - 1][col + 1]) {
+					next = gradientMatrix[row - 1][col + 1];
+				}
+				if (col != 0 && row + 1 < height && intensity >= gradientMatrix[row + 1][col -1]) {
+					prev = gradientMatrix[row + 1][col - 1];
+				}
+			}
+// Direction North-South
+			if ((direction >= 67.5 && direction <= 112.5) || (direction >= -112.5 && direction <= -67.5) ) {
+				if (row + 1 < width && intensity >= gradientMatrix[row+1][col]) {
+					next = gradientMatrix[row + 1][col];
+				}
+				if (row != 0 && intensity >= gradientMatrix[row-1][col]) {
+					prev = gradientMatrix[row - 1][col];
+				}
+			}
+// Direction North East-South West
+			if ((direction >= 112.5 && direction <= 157.5) || (direction >= -67.5 && direction <= -22.5) ) {
+				if (col + 1 < width && intensity >= gradientMatrix[row][col + 1]) {
+					next = gradientMatrix[row][col + 1];
+				}
+				if (col != 0 && intensity >= gradientMatrix[row][col -1]) {
+					prev = gradientMatrix[row][col - 1];
+				}
+			}
+			
+			if (intensity > prev && intensity > next) {
+				edgesMatrix[row][col] = 255;
+			}
+			else {
+				edgesMatrix[row][col] = 0;
+			}
+		}
+	}
 	std::vector<unsigned char> gradientIntensitiesVector;
 	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width ; col++) {
-			int dx = data[row * width * 4 + col * 4] - data[row * width * 4 + (col + 1) * 4];
-			int dy = data[row * width * 4 + col * 4] - data[(row+1) * width * 4 + col * 4];
-			int gradient = sqrt(dx * dx + dy * dy);
-			gradientIntensitiesVector.push_back(gradient);
-			gradientIntensitiesVector.push_back(gradient);
-			gradientIntensitiesVector.push_back(gradient);
+		for (int col = 0; col < width; col++) {
+			gradientIntensitiesVector.push_back(edgesMatrix[row][col]);
+			gradientIntensitiesVector.push_back(edgesMatrix[row][col]);
+			gradientIntensitiesVector.push_back(edgesMatrix[row][col]);
 			gradientIntensitiesVector.push_back(data[row * width * 4 + col * 4 + 3]);
 		}
 	}
+	
 	unsigned char* gradientIntensities;
 	gradientIntensities = (unsigned char*)malloc(gradientIntensitiesVector.size() * sizeof(unsigned char));
 	std::copy(gradientIntensitiesVector.begin(), gradientIntensitiesVector.end(), gradientIntensities);
@@ -98,12 +213,141 @@ unsigned char* Game::EdgeDetection(int width, int height, unsigned char* data)
 
 unsigned char* Game::Halftones(int width, int height, unsigned char* data)
 {
-	return data;
+	const int MAX_INTENSITY = 255;
+	const int NUM_INTENSITIES = 4;
+	// Make a 2d version of the data for easier handling.	
+	std::vector<std::vector<double>> data2d(height, std::vector<double>(width));
+
+	int max = 0;
+	int min = 255;
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			data2d[row][col] = data[row * width * 4 + col * 4];
+			if (data2d[row][col] > max) {
+				max = data2d[row][col];
+			}
+			if (data2d[row][col] < min) {
+				min = data2d[row][col];
+			}
+		}
+	}
+
+	// Tried improving contrast, didn't seem to do much.
+	double factor = 255.0 / (double)(max-min);
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			data2d[row][col] = trunc((double)(data2d[row][col] - min) * factor);
+		}
+			
+	}
+
+
+	std::vector<std::vector<int>> halftonedMatrix(height, std::vector<int>(width));
+
+	for (int row = 0; row < height-1; row+=2) {
+		for (int col = 0; col < width-1; col+=2) {
+			double average = (data2d[row][col] + data2d[row][col + 1] + data2d[row + 1][col] + data2d[row + 1][col + 1])/4.0;
+			int intensity = trunc(((double)average / (double)MAX_INTENSITY) * (double)NUM_INTENSITIES + 0.5);
+			switch (intensity) {
+			case 0:
+				halftonedMatrix[row][col] = 0;
+				halftonedMatrix[row][col+1] = 0;
+				halftonedMatrix[row+1][col] = 0;
+				halftonedMatrix[row+1][col+1] = 0;
+				break;
+			case 1:
+				halftonedMatrix[row][col] = 255;
+				halftonedMatrix[row][col+1] = 0;
+				halftonedMatrix[row+1][col] = 0;
+				halftonedMatrix[row+1][col+1] = 0;
+				break;
+			case 2:
+				halftonedMatrix[row][col] = 255;
+				halftonedMatrix[row][col+1] = 0;
+				halftonedMatrix[row+1][col] = 0;
+				halftonedMatrix[row+1][col+1] = 255;
+				break;
+			case 3:
+				halftonedMatrix[row][col] = 255;
+				halftonedMatrix[row][col+1] = 255;
+				halftonedMatrix[row+1][col] = 0;
+				halftonedMatrix[row+1][col+1] = 255;
+				break;
+			case 4:
+				halftonedMatrix[row][col] = 255;
+				halftonedMatrix[row][col+1] = 255;
+				halftonedMatrix[row+1][col] = 255;
+				halftonedMatrix[row+1][col+1] = 255;
+				break;
+			}
+		}
+	}
+	
+	std::vector<unsigned char> halftonedVector;
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			halftonedVector.push_back(halftonedMatrix[row][col]);
+			halftonedVector.push_back(halftonedMatrix[row][col]);
+			halftonedVector.push_back(halftonedMatrix[row][col]);
+			halftonedVector.push_back(data[row * width * 4 + col * 4 + 3]);
+		}
+	}
+
+	unsigned char* halftoned;
+	halftoned = (unsigned char*)malloc(halftonedVector.size() * sizeof(unsigned char));
+	std::copy(halftonedVector.begin(), halftonedVector.end(), halftoned);
+	return halftoned;
 }
 
 unsigned char* Game::FSAlgorithm(int width, int height, unsigned char* data)
 {
-	return data;
+	const int NUM_INTENSITIES = 15;
+	const int MAX_INTENSITY = 255;
+	// Make a 2d version of the data for easier handling.	
+	std::vector<std::vector<double>> data2d(height, std::vector<double>(width));
+
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			data2d[row][col] = data[row * width * 4 + col * 4];
+		}
+	}
+
+	std::vector<std::vector<int>> fsMatrix(height, std::vector<int>(width));
+
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			int intensity = trunc(((double)data2d[row][col] / (double)MAX_INTENSITY) * (double)NUM_INTENSITIES + 0.5);
+			fsMatrix[row][col] = trunc((double)intensity * ((double)MAX_INTENSITY / (double)NUM_INTENSITIES));
+			double error = data2d[row][col] - fsMatrix[row][col];
+			if (col + 1 < width) {
+				data2d[row][col + 1] += error * 7.0 / 16.0;
+			}
+			if (col != 0 && row + 1 < height) {
+				data2d[row + 1][col - 1] += error * 3.0 / 16.0;
+			}
+			if (row + 1 < height) {
+				data2d[row + 1][col] += error * 5.0 / 16.0;
+			}
+			if (col + 1 < width && row + 1 < height) {
+				data2d[row + 1][col + 1] += error * 1.0 / 16.0;
+			}
+		}
+	}
+
+	std::vector<unsigned char> fsVector;
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			fsVector.push_back(fsMatrix[row][col]);
+			fsVector.push_back(fsMatrix[row][col]);
+			fsVector.push_back(fsMatrix[row][col]);
+			fsVector.push_back(data[row * width * 4 + col * 4 + 3]);
+		}
+	}
+
+	unsigned char* fsalgo;
+	fsalgo = (unsigned char*)malloc(fsVector.size() * sizeof(unsigned char));
+	std::copy(fsVector.begin(), fsVector.end(), fsalgo);
+	return fsalgo;
 }
 
 void Game::Update(const glm::mat4 &MVP,const glm::mat4 &Model,const int  shaderIndx)
